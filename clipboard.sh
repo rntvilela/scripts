81bb92435850da2a4c178e0bfb779d5183adb557
rntvilela <renatovilela55@gmail.com>
2022-02-06 18:47:56 -0300

    auto commit

--- clipboard.sh
+++ clipboard.sh
@@ -10,6 +10,6 @@ usage() {
 
 case $1 in
     -w) echo "$(xclip -o -sel c)" >> /tmp/tmpclip.txt ;;
-    -r) cat /tmp/tmpclip.txt | dmenu -l 30 | tr -d '\n' | xclip -sel c ;;
+    -r) cat /tmp/tmpclip.txt | dmenu -l 30 | tr -d "\n" | xclip -sel c ;;
     *) usage ;;
 esac
