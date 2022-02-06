9b2963e65d8ce837024381a66c753877ef0db933
rntvilela <renatovilela55@gmail.com>
2022-02-06 18:20:29 -0300

    auto commit

---
+++ clipboard.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+usage() {    
+    echo "usage: ./clipboard.sh [option]"
+    echo ""
+    echo " -w write selection to tempclip.txt."
+    echo " -r read sekections from tempclip.txt."
+    echo " -h to show this menu."
+}
+
+case $1 in
+    -w) echo "$(xclip -o -sel c)" >> /tmp/tmpclip.txt ;;
+    -r) cat /tmp/tmpclip.txt | dmenu -l 30 | tr -d '\n' | xclip -sel c ;;
+    *) usage ;;
+esac
