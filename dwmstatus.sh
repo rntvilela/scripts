d73bc341fdc66d9d6a724ebe9cde809861a17089
rntvilela <renatovilela55@gmail.com>
2022-02-09 16:38:47 -0300

    auto commit

--- dwmstatus.sh
+++ dwmstatus.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-update_weather()
-{
-    weather=$(curl -Ss 'https://wttr.in?0&T&Q' | cut -c16- | head -2 | echo " $(xargs)")
-}
-
 update_cpu(){
     cpu=$(top -bn2 | grep '%Cpu' | tail -1 | awk '{print "CPU  " 100-$8 "%"}')
 }
 
 update_ram(){
-    ram=$(free -h | awk '/^M/ {print "RAM  " $3 " / " $2}')
+    ram=$(free -h | awk '/^M/ {print "RAM  " $3}')
 }
 
 update_brightness(){
@@ -34,14 +29,12 @@ update_clock(){
 }
 
 update_status(){
-        xsetroot -name " $weather | $cpu | $ram | $brightness | $audio | $battery | $clock"
+        xsetroot -name " $cpu | $ram | $brightness | $audio | $battery | $clock"
 }
 
 s=0
-
 update_audio && update_brightness && update_ram && update_clock && update_battery && update_status
 update_cpu && update_status
-update_weather && update_status
 
 while true; do
     sleep 1
@@ -49,7 +42,6 @@ while true; do
     
     update_audio && update_brightness
     [ $((s%15)) = 0 ] && update_cpu && update_ram
-    [ $((s%60)) = 0 ] && update_clock && update_battery 
-    [ $s = 3600 ] && update_weather && s=0
+    [ $s = 60 ] && update_clock && update_battery && s=0
     update_status    
 done
