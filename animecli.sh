876f6d4e640a0c53e4975d63f007648fc3e604b8
rntvilela <renatovilela55@gmail.com>
2022-02-13 11:39:42 -0300

    auto commit

--- animecli.sh
+++ animecli.sh
@@ -16,15 +16,17 @@ else
     [ -n "$anime_escolhido" ] && episodio=$(curl -s "https://animesonline.cc/anime/$anime_escolhido" | grep -Eo "https:\/\/animesonline.cc\/episodio\/[a-zA-Z0-9-]*/" | uniq | cut -c34- | dmenu -l 30)
 fi
 
-[ -n "$episodio" ] && mpv "https://animesonline.cc/$episodio"
-
-if [ "$?" = "0" ]
+if [ -n "$episodio" ]
 then
-    echo -n "Você acabou de assistir: "
-    echo "$episodio." | tr -d '/' | tr '-' ' ' | sed 's/\<./\U&/g'
-else
-    echo -n "Falha ao executar com mpv, abrindo: "
-    echo -n "$episodio " | tr -d '/' | tr '-' ' ' | sed 's/\<./\U&/g'
-    echo "com $browser."
-    $browser "https://animesonline.cc/episodio/$episodio"
+    mpv "https://animesonline.cc/$episodio"
+    if [ $? = 0 ]
+    then
+        echo -n "Você acabou de assistir: "
+        echo "$episodio." | tr -d '/' | tr '-' ' ' | sed 's/\<./\U&/g'
+    else
+        echo -n "Falha ao executar com mpv, abrindo: "
+        echo -n "$episodio " | tr -d '/' | tr '-' ' ' | sed 's/\<./\U&/g'
+        echo "com $browser."
+        $browser "https://animesonline.cc/episodio/$episodio"
+    fi
 fi
