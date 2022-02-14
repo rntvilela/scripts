5e97e030683eb98b25ac5926133f03190c8755a7
rntvilela <renatovilela55@gmail.com>
2022-02-14 15:48:42 -0300

    auto commit

---
+++ tux.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+usage() {
+    echo "usage: ./tux.sh [option]"
+    echo ""
+    echo " -c to calcule expression]"
+    echo " -t to translate word"
+    echo " -s to search for sentence"
+}
+
+case $1 in
+    -c) echo "scale=2; $2" | bc -l ;;
+    *) usage ;;
+esac
+
