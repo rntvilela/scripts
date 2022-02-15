09b593202f7ab487efc34f768b7539330ea37bbb
rntvilela <renatovilela55@gmail.com>
2022-02-14 22:51:06 -0300

    auto commit

--- tux.sh
+++ tux.sh
@@ -3,13 +3,18 @@
 usage() {
     echo "usage: ./tux.sh [option]"
     echo ""
+    echo " -d to get date"
     echo " -c to calcule expression"
     echo " -t to translate word"
     echo " -s to search for sentence"
+    echo " -w to get weather"
+
 }
 
 case $1 in
     -c) echo "scale=2; $2" | bc -l ;;
+    -d) echo "$(date +'%a, %d %b %y, %H:%M' && echo && cal)" ;;
+    -w) echo "$(curl -Ss 'https://wttr.in/?Q&T' | cut -c16- | head -2 | xargs)" ;;
     *) usage ;;
 esac
 
