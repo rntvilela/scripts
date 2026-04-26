4f319f68f97ce6e0c2115a83d046199405e47f6a
rntvilela <renatovilela55@gmail.com>
2026-04-26 15:45:55 -0300

    Create directory before update

--- update_config_files.sh
+++ update_config_files.sh
@@ -4,6 +4,18 @@ home="/home/renato"
 suckless="${home}/suckless"
 config_files="${home}/config_files"
 
+mkdir -p ${config_files}/dwm/
+mkdir -p ${config_files}/st/
+mkdir -p ${config_files}/dmenu/
+
+mkdir -p ${config_files}/bash/
+mkdir -p ${config_files}/vim/
+mkdir -p ${config_files}/tmux/
+
+mkdir -p ${config_files}/x11/
+mkdir -p ${config_files}/x11/
+mkdir -p ${config_files}/x11/
+
 cp ${suckless}/dwm/config.h   ${config_files}/dwm/
 cp ${suckless}/st/config.h    ${config_files}/st/
 cp ${suckless}/dmenu/config.h ${config_files}/dmenu/
